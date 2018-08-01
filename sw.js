d2f62af5472610a9a13992fc8b781000bb83eef5
Oriane <>
2018-07-31 21:44:30 -0400

    Cache map api requests as they are fetched

--- sw.js
+++ sw.js
@@ -7,9 +7,9 @@ Service worker script
 let staticCacheName = 'restaurant-reviews-v1';
 
 /* Handle service worker installation and cache responses to requests for site assets */
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(staticCacheName).then(function(cache) {
+        caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
                 './css/styles.css',
                 './css/small.css',
@@ -37,13 +37,13 @@ self.addEventListener('install', function(event) {
 })
 
 /* Handle new service worker activation with updated cached site assets */
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                cacheNames.filter(function(cacheName) {
+                cacheNames.filter(function (cacheName) {
                     return cacheName.startsWith('restaurant-reviews-') && cacheName != staticCacheName;
-                }).map(function(cacheName) {
+                }).map(function (cacheName) {
                     return cache.delete(cacheName);
                 })
             )
@@ -51,15 +51,31 @@ self.addEventListener('activate', function(event) {
     )
 })
 
-/* Handle service worker fetch events with matching request to cached response or displaying offline message if unavailable */
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) return response;
-            return fetch(event.request);
-        }).catch(function() {
-            fetch('./offline.html');
-        })
-    )
+/* Handle service worker fetch events with matching request to cached response */
+self.addEventListener('fetch', function (event) {
+
+    let mapUrl = 'https://api.tiles.mapbox.com/v4/mapbox.streets/';
+
+    // Cache all map api related requests as they are made (code block reference https://samdutton.github.io/ilt/pwa/data/)
+    if (event.request.url.indexOf(mapUrl) === 0) {
+        event.respondWith(
+            fetch(event.request)
+                .then(function (response) {
+                    return caches.open(staticCacheName).then(function (cache) {
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                })
+        );
+    // return matching cache when offline and display offline message if failed
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            }).catch(function () {
+                fetch('./offline.html');
+            })
+        )
+    }
 })
 
